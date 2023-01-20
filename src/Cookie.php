fa489d26c99cc57e9bbc6eec863f415308617420
commandstring <bmxmikey2@gmail.com>
2023-01-20 02:13:00 -0500

    Compatible with React/Http

--- src/Cookie.php
+++ src/Cookie.php
@@ -2,97 +2,43 @@
 
 namespace CommandString\Cookies;
 
-use InvalidArgumentException;
-use CommandString\Cookies\NullEncryption;
+use CommandString\Cookies\CookieController;
+use Psr\Http\Message\ResponseInterface;
+use React\Http\Message\ServerRequest;
 
-/**
- * An simpler way to manipulate cookies in PHP
- * 
- * @author Command_String - https://discord.dog/232224992908017664
- */
 class Cookie {
-    private cookieEncryptionInterface $encryptor;
-        
-    public function __construct(?cookieEncryptionInterface $encryptor = null)
+    public function __construct(
+        private ServerRequest &$request,
+        private ResponseInterface &$response,
+        private CookieController $controller
+    ) {}
+
+    public function exists($name): bool
     {
-        $this->encryptor = $encryptor ?? new nullEncryption;
+        return $this->controller->exists($this->request, $name);
     }
 
-    /**
-     * Does cookie exist
-     * 
-     * @param string $name
-     * 
-     * @return bool
-     */
-    public function exists(string $name):bool
+    public function set(string $name, string|int $value, int $hoursValid=168, int $minutesValid=0, int $secondsValid=0, string $path = "/", string $domain = "", bool $secure = false, bool $httponly = false): self 
     {
-        return isset($_COOKIE[$name]);
-    }
-
-    /**
-     * Set a cookie
-     * 
-     * @param string $name
-     * @param string|int $value
-     * @param int $hoursValid
-     * @param int $minutesValid
-     * @param int $secondsValid
-     * @param string $path
-     * @param string $domain
-     * @param bool $secure
-     * @param bool $httponly
-     * @return cookie
-     */
-    public function set(string $name, string|int $value, int $hoursValid=168, int $minutesValid=0, int $secondsValid=0, string $path = "/", string $domain = "", bool $secure = false, bool $httponly = false):cookie
-    {
-        $encryptedString = $this->encryptor->encrypt($value);
-        setcookie($name, $encryptedString, time() + ((3600 * $hoursValid) + (60 * $minutesValid) + $secondsValid), $path, $domain, $secure, $httponly);
-
-        $_COOKIE[$name] = $encryptedString;
-
+        $this->controller->set($this->response, $name, $value, $hoursValid, $minutesValid, $secondsValid, $path, $domain, $secondsValid, $httponly);
         return $this;
     }
 
-    /**
-     * Get cookie
-     * 
-     * @param string $name
-     * 
-     * @return ?string
-     */
-    public function get(string $name): ?string
+    public function get($name): ?string
     {
-        return $this->encryptor->decrypt($_COOKIE[$name]) ?? null;
+        return $this->controller->get($this->request, $name);
     }
 
-    /**
-     * Delete cookie(s) specified
-     * 
-     * @param string $name
-     */
-    public function delete(string ...$cookies):cookie
+    public function delete(string $cookie, string $path = "/", string $domain = ""): self
     {
-        foreach ($cookies as $cookie) {
-            if ($this->exists($_COOKIE[$cookie])) {
-                throw new InvalidArgumentException("Cookie doesn't exist in configuration");
-            }
-
-            unset($_COOKIE[$cookie]);
-            setcookie($cookie, null, -1, '/');
-        }
-
+        $this->controller->delete($this->response, $this->request, $cookie, $path, $domain);
         return $this;
     }
 
-    /**
-     * Deletes all cookies
-     * 
-     * @return void
-     */
-    public function deleteAll():cookie 
+    public function deleteAll(): self
     {
-        $this->delete(...array_keys($_COOKIE));
+        $this->controller->deleteAll($this->response, $this->request);
+
         return $this;
     }
 }
